3628db57dd91996cb53d583fa7f616bc396afe43
Xuan Canh <xuancanhit99@gmail.com>
2021-02-23 22:52:20 +0300

    update 4 file

---
+++ dbconnection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'hrmsdb';
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection Fail: " . mysqli_connect_error());
+}
+
+#this is comment
+#aaaaaaaaaaaaaacccccccccc
+#123
